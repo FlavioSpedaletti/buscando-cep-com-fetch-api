492efe76ad3306ba9b55c27fb722557c8aa2a458
Flavio Spedaletti <flavio.hfs@gmail.com>
2019-08-05 11:55:55 -0300

    consulta no site viacep e busca de cep automático por geolocation + openstreet

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,32 @@
+window.addEventListener( 'load', function(event) {
+    geolocalizar();
+});
+
+function geolocalizar() {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function(posicao) {
+            var url = "http://nominatim.openstreetmap.org/reverse?lat="+posicao.coords.latitude+"&lon="+posicao.coords.longitude+"&format=json&json_callback=preencherCep";
+
+            var script = document.createElement('script');
+            script.src = url;
+            document.body.appendChild(script);
+        });
+    } else {
+        alert('Seu navegador não suporta geolocation!');
+    }
+}
+
+function preencherCep(dados) {
+    document.querySelector('#cep').value = dados.address.postcode;
+}
+
 function buscarCep(event, form) {
     event.preventDefault();
     const inputCep = form.cep;
     if (inputCep) {
         const cep = inputCep.value;
         if (cep.length === 8) {
-            const URL = `http://ws.matheuscastiglioni.com.br/ws/cep/find/${cep}/json`;
+            const URL = `https://viacep.com.br/ws/${cep}/json`;
             fetch(URL)
                 .then(resposta => resposta.json())
                 .then(data => mostrarResposta(data))
@@ -15,12 +37,12 @@ function buscarCep(event, form) {
 
 function mostrarResposta(cep) {
     const mensagem = `
-        CEP: ${cep.cep},
-        Logradouro: ${cep.logradouro},
-        Complemento: ${cep.complemento},
-        Bairro: ${cep.bairro},
-        Cidade ${cep.cidade},
-        Estado: ${cep.estado}
+        CEP: ${cep.cep}
+        Logradouro: ${cep.logradouro}
+        Complemento: ${cep.complemento}
+        Bairro: ${cep.bairro}
+        Cidade ${cep.localidade}
+        Estado: ${cep.uf}
     `;
     alert(mensagem);
 }
